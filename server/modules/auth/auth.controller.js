671772b42c0be0c000d297d2f6bad75205b48381
Nishant3105 <nishantkumar2522@gmail.com>
2026-02-18 23:32:48 +0530

    Backend ready for prod

--- server/modules/auth/auth.controller.js
+++ server/modules/auth/auth.controller.js
@@ -9,7 +9,7 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
 };
 
 const generateAccessToken = (user) => {
